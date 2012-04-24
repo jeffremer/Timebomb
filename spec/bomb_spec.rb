b206332c8435d4c97dd19fea4bddf16dac5cdd6a
Jeff Remer <jeff@strava.com>
2012-04-23 17:38:39 -0700

    Hardcoded sleep and result

--- spec/bomb_spec.rb
+++ spec/bomb_spec.rb
@@ -1,12 +1,7 @@
 describe "Timebomb" do
-  before :all do
-    sleep ENV["SLEEP"].to_i if ENV["SLEEP"]
-  end
-  
   it 'should produce a result' do
-    puts "ENV[RESULT] = #{ENV['RESULT']}"
-    result = ENV['RESULT'] == "FAILURE" ? false : true
-    true.should == result
+    sleep 5
+    true.should == false
   end
 end
     
